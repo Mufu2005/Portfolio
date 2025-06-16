24a6dadcd78ca4a97ee9956eced4f1590662ee75
Mufu2005 <mufumustali@gmail.com>
2025-06-17 01:26:06 +0500

    scroll animations testing

--- FrontEnd/FrontEnd/obj/Debug/net8.0/FrontEnd.AssemblyInfo.cs
+++ FrontEnd/FrontEnd/obj/Debug/net8.0/FrontEnd.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a71ccd7edaa03e6da7895feeec82433775657024")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f51877cd4162f1dc436d7b4712e2cffa7e446ef4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
