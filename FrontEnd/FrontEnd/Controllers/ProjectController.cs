dba44b6b064c48db48fdca9171cada3bfc64cf2c
Mufu2005 <mufumustali@gmail.com>
2025-07-07 04:19:11 +0500

    Projects(UserView)

---
+++ FrontEnd/FrontEnd/Controllers/ProjectController.cs
@@ -0,0 +1,22 @@
+﻿using FrontEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEnd.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private String projectBaseUrl = "http://localhost:5053/";
+        private HttpClient _projectClient;
+
+        public ProjectController(IHttpClientFactory factory)
+        {
+            _projectClient = factory.CreateClient();
+            _projectClient.BaseAddress = new Uri(projectBaseUrl);
+        }
+        public async Task<IActionResult> Index()
+        {
+            var projects = await _projectClient.GetFromJsonAsync<List<ProjectModel>>("api/Project/list");
+            return View(projects);
+        }
+    }
+}
