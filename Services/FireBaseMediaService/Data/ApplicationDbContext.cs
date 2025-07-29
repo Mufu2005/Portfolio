a8fb46a7475fb5562f1b3ed2ce4b5ad528f2b30f
Mufu2005 <mufumustali@gmail.com>
2025-07-29 07:25:37 +0500

    FirebaseMediaService(complete)

---
+++ Services/FireBaseMediaService/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FirebaseMediaService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirebaseMediaService.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<MediaDbModel> MediaTable { get; set; }
+    }
+}
---
+++ Services/FireBaseMediaService/Models/MediaDbModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FirebaseMediaService.Models
+{
+    public class MediaDbModel
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Url  { get; set; }
+        public string? Folder { get; set; }
+
+    }
+}
